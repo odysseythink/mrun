35bff884f7f922a7937625e98abc6fb3dffac4f9
冉威 <ranwei@ranwei.com>
2023-09-12 17:17:06 +0800

    hashset使用简单的map

--- containers/sets/hashset/hashset.go
+++ containers/sets/hashset/hashset.go
@@ -1,19 +1,9 @@
-// Copyright (c) 2015, Emir Pasic. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Package hashset implements a set backed by a hash table.
-//
-// Structure is not thread safe.
-//
-// References: http://en.wikipedia.org/wiki/Set_%28abstract_data_type%29
 package hashset
 
 import (
 	"fmt"
 	"strings"
 
-	"mlib.com/mrun/containers"
 	"mlib.com/mrun/containers/sets"
 )
 
@@ -22,14 +12,14 @@ var _ sets.Set[int] = (*Set[int])(nil)
 
 // Set holds elements in go's native map
 type Set[T comparable] struct {
-	items containers.SyncMap[T, struct{}]
+	items map[T]struct{}
 }
 
 var itemExists = struct{}{}
 
 // New instantiates a new empty set and adds the passed values, if any, to the set
 func New[T comparable](values ...T) *Set[T] {
-	set := &Set[T]{}
+	set := &Set[T]{items: make(map[T]struct{})}
 	if len(values) > 0 {
 		set.Add(values...)
 	}
@@ -39,14 +29,14 @@ func New[T comparable](values ...T) *Set[T] {
 // Add adds the items (one or more) to the set.
 func (set *Set[T]) Add(items ...T) {
 	for _, item := range items {
-		set.items.Store(item, itemExists)
+		set.items[item] = itemExists
 	}
 }
 
 // Remove removes the items (one or more) from the set.
 func (set *Set[T]) Remove(items ...T) {
 	for _, item := range items {
-		set.items.Delete(item)
+		delete(set.items, item)
 	}
 }
 
@@ -55,7 +45,7 @@ func (set *Set[T]) Remove(items ...T) {
 // Returns true if no arguments are passed at all, i.e. set is always superset of empty set.
 func (set *Set[T]) Contains(items ...T) bool {
 	for _, item := range items {
-		if _, contains := set.items.Load(item); !contains {
+		if _, contains := set.items[item]; !contains {
 			return false
 		}
 	}
@@ -69,22 +59,21 @@ func (set *Set[T]) Empty() bool {
 
 // Size returns number of elements within the set.
 func (set *Set[T]) Size() int {
-	return set.items.Len()
+	return len(set.items)
 }
 
 // Clear clears all values in the set.
 func (set *Set[T]) Clear() {
-	set.items.Clear()
+	set.items = make(map[T]struct{})
 }
 
 // Values returns all items in the set.
 func (set *Set[T]) Values() []T {
 	values := make([]T, set.Size())
 	count := 0
-	set.items.Range(func(key T, value struct{}) bool {
-		values[count] = key
-		return true
-	})
+	for k := range set.items {
+		values[count] = k
+	}
 	return values
 }
 
@@ -92,10 +81,10 @@ func (set *Set[T]) Values() []T {
 func (set *Set[T]) String() string {
 	str := "HashSet\n"
 	items := []string{}
-	set.items.Range(func(key T, value struct{}) bool {
-		items = append(items, fmt.Sprintf("%v", key))
-		return true
-	})
+	for k := range set.items {
+		items = append(items, fmt.Sprintf("%v", k))
+	}
+
 	str += strings.Join(items, ", ")
 	return str
 }
@@ -108,19 +97,17 @@ func (set *Set[T]) Intersection(another *Set[T]) *Set[T] {
 
 	// Iterate over smaller set (optimization)
 	if set.Size() <= another.Size() {
-		set.items.Range(func(key T, value struct{}) bool {
-			if _, contains := another.items.Load(key); contains {
-				result.Add(key)
+		for k := range set.items {
+			if _, contains := another.items[k]; contains {
+				result.Add(k)
 			}
-			return true
-		})
+		}
 	} else {
-		another.items.Range(func(key T, value struct{}) bool {
-			if _, contains := set.items.Load(key); contains {
-				result.Add(key)
+		for k := range another.items {
+			if _, contains := set.items[k]; contains {
+				result.Add(k)
 			}
-			return true
-		})
+		}
 	}
 
 	return result
@@ -131,15 +118,12 @@ func (set *Set[T]) Intersection(another *Set[T]) *Set[T] {
 // Ref: https://en.wikipedia.org/wiki/Union_(set_theory)
 func (set *Set[T]) Union(another *Set[T]) *Set[T] {
 	result := New[T]()
-
-	set.items.Range(func(key T, value struct{}) bool {
-		result.Add(key)
-		return true
-	})
-	another.items.Range(func(key T, value struct{}) bool {
-		result.Add(key)
-		return true
-	})
+	for k := range set.items {
+		result.Add(k)
+	}
+	for k := range another.items {
+		result.Add(k)
+	}
 
 	return result
 }
@@ -149,12 +133,10 @@ func (set *Set[T]) Union(another *Set[T]) *Set[T] {
 // Ref: https://proofwiki.org/wiki/Definition:Set_Difference
 func (set *Set[T]) Difference(another *Set[T]) *Set[T] {
 	result := New[T]()
-
-	set.items.Range(func(key T, value struct{}) bool {
-		if _, contains := another.items.Load(key); !contains {
-			result.Add(key)
+	for k := range set.items {
+		if _, contains := another.items[k]; !contains {
+			result.Add(k)
 		}
-		return true
-	})
+	}
 	return result
 }
