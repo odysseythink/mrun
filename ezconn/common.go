c7ee32e603cab404a2e71ace097412079210b57e
george <george@george.com>
2025-09-24 14:17:40 +0800

    modify

---
+++ ezconn/common.go
@@ -0,0 +1,42 @@
+package ezconn
+
+import (
+	"log"
+	"net"
+	"runtime"
+	"strconv"
+)
+
+func IsNetAddrValid(addr string) bool {
+	host, port, err := net.SplitHostPort(addr)
+	if err != nil {
+		log.Printf("[E]addr(%s)have the invalid format:%v\n", addr, err)
+		return false
+	}
+	if ip := net.ParseIP(host); ip == nil {
+		log.Printf("[E]net.ParseIP(%s) failed:%v\n", host, err)
+		return false
+	}
+
+	iport, err := strconv.Atoi(port)
+	if err != nil {
+		log.Printf("[E]port(%s) convert to int failed:%v\n", port, err)
+		return false
+	}
+
+	if iport < 0 || iport > 65535 {
+		log.Printf("[E]port(%d) must in range:0~65535\n", iport)
+		return false
+	}
+	return true
+}
+
+var ms runtime.MemStats
+
+func DebugMem() {
+	runtime.GC()
+	runtime.ReadMemStats(&ms)
+	// fmt.Printf("-----%+v\n", m)
+	// fmt.Printf("-----os %d\n", m.Sys)
+	log.Printf("------sys:%d Alloc:%d(bytes) HeapAlloc:%d(bytes) HeapIdle:%d(bytes) HeapReleased:%d(bytes)\n", ms.Sys, ms.Alloc, ms.HeapAlloc, ms.HeapIdle, ms.HeapReleased)
+}
